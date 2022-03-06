1d073ab46806d97c19af93b8d70564ac67fe0ef7
rakaar <rka87338@gmail.com>
2022-03-06 11:40:22 +0530

    q2 err

--- pakka_q1.m
+++ pakka_q1.m
@@ -181,3 +181,56 @@ figure
         legend('500 hz','4 khz');
     hold off
 grid
+
+%% ================== q2 ===================
+[fivewo, fs] = audioread('fivewo.wav');   %reading entire wav file
+ah = fivewo(98250:113500);    % 'ah' sound
+sound(ah, fs);
+rms_ah = rms(ah);
+I_0 = 20*10^(-6);
+db_SPL = 20*log10(rms_ah/I_0);
+% what is use of RMS??? - mulitply later to feed into ANFs
+intensities = -20:10:80;
+ah = ah';
+
+CF    = 500; % CF in Hz;   
+cohc  = 1.0;   % normal ohc function
+cihc  = 1.0;   % normal ihc function
+fiberType = 3; % spontaneous rate (in spikes/s) of the fiber BEFORE refractory effects; "1" = Low; "2" = Medium; "3" = High
+implnt = 0;    % "0" for approximate or "1" for actual implementation of the power-law functions in the Synapse
+% stimulus parameters
+F0 = CF;     % stimulus frequency in Hz
+Fs = 100e3;  % sampling rate in Hz (must be 100, 200 or 500 kHz)
+T  = length(ah);  % stimulus duration in seconds
+rt = 5e-3;   % rise/fall time in seconds
+% PSTH parameters
+nrep = 1;               % number of stimulus repetitions (e.g., 50);
+psthbinwidth = 0.5e-3; % binwidth in seconds;
+
+t = 0:1/Fs:T-1/Fs; % time vector
+mxpts = length(t);
+irpts = rt*Fs;
+
+rate_vs_intensity_500 = zeros(1, length(intensities));
+for i=1:length(intensities)
+    stimdb = intensities(i);
+    pin = ah; % unramped stimulus - ah
+    pin(1:irpts)=pin(1:irpts).*(0:(irpts-1))/irpts; 
+    pin((mxpts-irpts):mxpts)=pin((mxpts-irpts):mxpts).*(irpts:-1:0)/irpts;
+    
+    vihc = catmodel_IHC(pin,CF,nrep,1/Fs,T*2,cohc,cihc); 
+    [synout,psth] = catmodel_Synapse(vihc,CF,nrep,1/Fs,fiberType,implnt); 
+    
+    timeout = (1:length(psth))*1/Fs;
+    psthbins = round(psthbinwidth*Fs);  % number of psth bins per psth bin
+    psthtime = timeout(1:psthbins:end); % time vector for psth
+    pr = sum(reshape(psth,psthbins,length(psth)/psthbins))/nrep; % pr of spike in each bin
+    Psth = pr/psthbinwidth; % psth in units of spikes/s
+    Psth_avg = sum(Psth)/length(Psth);
+    rate_vs_intensity_500(1, i) = Psth_avg;
+end
+
+figure
+    plot(intensities,rate_vs_intensity_500)
+    title('rate vs intensity for ah sound -500 hz BF')
+grid
